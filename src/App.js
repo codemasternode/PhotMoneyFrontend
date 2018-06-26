58742d42b0244b2dcbc23853e929d204538ee1fc
codemasternode <marcinwarzybok@outlook.com>
2018-06-26 11:00:40 +0200

    root

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Loadable from 'react-loadable';
+import { connect } from 'react-redux';
+
+import './App.css';
+
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+}
+
+export default connect()(App);
